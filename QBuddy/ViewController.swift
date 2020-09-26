2dd1c2113cb8a45575d423ccc09093b76dac5905
Pinxuan Huang <pihu@pinxuans-mac.local>
2020-09-26 16:42:06 -0400

    Add components to login screen

--- QBuddy/ViewController.swift
+++ QBuddy/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     
     var logoLabel: UILabel!
     var logoImageView: UIImageView!
+    var loginLabel: UILabel!
+    var emailField: UITextField!
+    var passwordField: UITextField!
     var loginButton: UIButton!
     var orLabel: UILabel!
     var signUpLabel: UILabel!
@@ -25,20 +28,49 @@ class ViewController: UIViewController {
         logoLabel.text = "QBuddy"
         logoLabel.textColor = UIColor(red:115/255, green:125/255, blue:254/255, alpha: 1.0)
         logoLabel.font = UIFont.systemFont(ofSize: 26) //todo: set font
+        logoLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(logoLabel)
         
         logoImageView = UIImageView()
         view.addSubview(logoImageView)
         
+        loginLabel = UILabel()
+        loginLabel.text = "Login"
+        loginLabel.textColor = UIColor(red:115/255, green:125/255, blue:254/255, alpha: 1.0)
+        loginLabel.font = UIFont.systemFont(ofSize: 16)
+        loginLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loginLabel)
+        
+        emailField = UITextField()
+//        emailField.text
+        emailField.backgroundColor = .white
+        emailField.clearsOnBeginEditing = false
+        emailField.borderStyle = .roundedRect
+        emailField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(emailField)
+        
+        passwordField = UITextField()
+        //        passwordField
+        passwordField.backgroundColor = .white
+        passwordField.clearsOnBeginEditing = false
+        passwordField.borderStyle = .roundedRect
+        passwordField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(passwordField)
+        
         loginButton = UIButton()
+        loginButton.setTitle("Login", for:.normal)
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(loginButton)
         
         orLabel = UILabel()
         orLabel.text = "OR"
+        orLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(orLabel)
         
         signUpLabel = UILabel()
         signUpLabel.text = "sign up"
+        signUpLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(signUpLabel)
         
     }
