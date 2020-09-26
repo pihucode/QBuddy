b5e16eb297e27c1bf7fbff99ab4ef7758a2e1590
Pinxuan Huang <pihu@pinxuans-mac.local>
2020-09-26 15:32:52 -0400

    Initial Commit

---
+++ QBuddyTests/QBuddyTests.swift
@@ -0,0 +1,34 @@
+//
+//  QBuddyTests.swift
+//  QBuddyTests
+//
+//  Created by Pinxuan Huang on 9/26/20.
+//  Copyright © 2020 DTI. All rights reserved.
+//
+
+import XCTest
+@testable import QBuddy
+
+class QBuddyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
