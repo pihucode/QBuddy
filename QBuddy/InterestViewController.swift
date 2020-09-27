98c774696f209d48a914b7452fecb2fc809fddbb
Pinxuan Huang <pihu@pinxuans-mac.local>
2020-09-26 23:32:00 -0400

    Style interest view

--- QBuddy/InterestViewController.swift
+++ QBuddy/InterestViewController.swift
@@ -37,14 +37,14 @@ class InterestViewController: UIViewController {
         titleLabel = UILabel()
         titleLabel.text = "About yourself..."
         titleLabel.textColor = UIColor.darkBlueColor()
-        titleLabel.font = UIFont.systemFont(ofSize: 26) //todo: set font
+        titleLabel.font = UIFont(name: "Roboto", size: 30)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(titleLabel)
 
         subLabel = UILabel()
         subLabel.text = "Who do you prefer to pair with?"
         subLabel.textColor = UIColor.darkBlueColor()
-        subLabel.font = UIFont.systemFont(ofSize: 26) //todo: set font
+        subLabel.font = UIFont(name: "Roboto", size: 24)
         subLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(subLabel)
         
@@ -52,7 +52,7 @@ class InterestViewController: UIViewController {
         nameField = UITextField()
         nameField.placeholder = "Your name"
         nameField.backgroundColor = .white
-        nameField.clearsOnBeginEditing = false
+        nameField.clearsOnBeginEditing = true
         nameField.borderStyle = .roundedRect
         nameField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(nameField)
@@ -60,7 +60,7 @@ class InterestViewController: UIViewController {
         interestField = UITextField()
         interestField.placeholder = "Hobbies/Interest"
         interestField.backgroundColor = .white
-        interestField.clearsOnBeginEditing = false
+        interestField.clearsOnBeginEditing = true
         interestField.borderStyle = .roundedRect
         interestField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(interestField)
@@ -68,7 +68,7 @@ class InterestViewController: UIViewController {
         activityField = UITextField()
         activityField.placeholder = "What is your favorite quarantine activity?"
         activityField.backgroundColor = .white
-        activityField.clearsOnBeginEditing = false
+        activityField.clearsOnBeginEditing = true
         activityField.borderStyle = .roundedRect
         activityField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(activityField)
@@ -76,41 +76,83 @@ class InterestViewController: UIViewController {
         //Choices
         interestButton = UIButton()
         interestButton.setTitle("Same Interest", for:.normal)
-        interestButton.setTitleColor(.gray, for: .normal)
+        interestButton.setTitleColor(.white, for: .normal)
         interestButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(interestButton)
         
         yearButton = UIButton()
         yearButton.setTitle("Same Year", for:.normal)
-        yearButton.setTitleColor(.gray, for: .normal)
+        yearButton.setTitleColor(.white, for: .normal)
         yearButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(yearButton)
         
         majorButton = UIButton()
         majorButton.setTitle("Major", for:.normal)
-        majorButton.setTitleColor(.gray, for: .normal)
+        majorButton.setTitleColor(.white, for: .normal)
         majorButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(majorButton)
         
         hobbyButton = UIButton()
         hobbyButton.setTitle("Same Hobby", for:.normal)
-        hobbyButton.setTitleColor(.gray, for: .normal)
+        hobbyButton.setTitleColor(.white, for: .normal)
+        hobbyButton.layer.backgroundColor = UIColor.buttonChoiceColor().cgColor
         hobbyButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(hobbyButton)
+//        view.addSubview(hobbyButton)
         
         meetButton = UIButton()
         meetButton.setTitle("Meet", for:.normal)
-        meetButton.setTitleColor(UIColor.redColor(), for: .normal)
+        meetButton.setTitleColor(UIColor.lightGrayColor(), for: .normal)
+        meetButton.layer.backgroundColor = UIColor.redColor().cgColor
         meetButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(meetButton)
         
+        setupConstraints()
     }
     
     func setupConstraints() {
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24),
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
             titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
             ])
+        NSLayoutConstraint.activate([
+            nameField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+            nameField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 36),
+            nameField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -36),
+            nameField.heightAnchor.constraint(equalToConstant: 36)
+            ])
+        NSLayoutConstraint.activate([
+            interestField.topAnchor.constraint(equalTo: nameField.bottomAnchor, constant: 16),
+            interestField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 36),
+            interestField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -36),
+            interestField.heightAnchor.constraint(equalToConstant: 36)
+            ])
+        NSLayoutConstraint.activate([
+            activityField.topAnchor.constraint(equalTo: interestField.bottomAnchor, constant: 16),
+            activityField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 36),
+            activityField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -36),
+            activityField.heightAnchor.constraint(equalToConstant: 36)
+            ])
+        NSLayoutConstraint.activate([
+            subLabel.topAnchor.constraint(equalTo: activityField.bottomAnchor, constant: 32),
+            subLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            ])
+        NSLayoutConstraint.activate([
+            interestButton.topAnchor.constraint(equalTo: subLabel.bottomAnchor, constant: 16),
+            interestButton.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 52)
+            ])
+        NSLayoutConstraint.activate([
+            yearButton.topAnchor.constraint(equalTo: subLabel.bottomAnchor, constant: 16),
+            yearButton.leftAnchor.constraint(equalTo: interestButton.rightAnchor, constant: 32)
+            ])
+        NSLayoutConstraint.activate([
+            interestButton.topAnchor.constraint(equalTo: subLabel.bottomAnchor, constant: 16),
+            interestButton.leftAnchor.constraint(equalTo: yearButton.rightAnchor, constant: 32)
+            ])
+        NSLayoutConstraint.activate([
+            meetButton.topAnchor.constraint(equalTo: interestButton.bottomAnchor, constant: 52),
+            meetButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            ])
+        
     }
     
     
